0a419dfc8e5b4868ea62c0a730752da6ccfc67db
Vujihhu <flusy2017@yandex.ru>
2019-12-05 22:30:10 +0300

    Scale & Instantiate

---
+++ VR Daidokoro/Assets/Scripts for Prefab/Soforpref.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Soforpref : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject myPrefab;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [System.Obsolete]
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.name == "Knife2")
+        {
+            Destroy(gameObject);
+            Instantiate(myPrefab, new Vector3(gameObject.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z), Quaternion.identity);
+        }
+    }
+
+    
+}
